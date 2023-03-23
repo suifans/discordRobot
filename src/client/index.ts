68d3f1c409970c8c2f3fe4a5b3b078531068857e
henryliu <17600047539@163.com>
2023-03-24 00:50:10 +0800

    update

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,7 +2,8 @@ import {HttpClient} from "tsrpc-browser";
 import {serviceProto} from "../shared/protocols/serviceProto";
 
 const client = new HttpClient(serviceProto, {
-    server: 'http://localhost:3001',
+    // server: 'http://localhost:3001',
+    server: 'https://api.suirobots.com',
     json: true,
     // logger: console
 });
